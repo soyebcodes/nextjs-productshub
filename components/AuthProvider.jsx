78704dade3f413600870d61f176f0c360a3ed285
soyebcodes <soyeb.islam117@gmail.com>
2025-08-22 16:20:54 +0600

    add auth provider wrapper

--- components/AuthProvider.jsx
+++ components/AuthProvider.jsx
@@ -1,6 +1,8 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
 
-export default function Providers({ children }) {
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+export default function AuthProvider({ children }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
